cd61c9ee0f95ea76704a98efbb170a0994fe85ae
Prakash Ramamurthy (WT01 - TT Architect Academy) <prakash.ramamurthy@wipro.com>
2016-05-10 15:53:44 +0530

    changes made

--- hello.c
+++ hello.c
@@ -1,5 +1,11 @@
 //This is an attempt to put hello-world program here
 
-//here we write the code for it
+//here we write the code for software
 
 //here we fill data for the code
+
+//started adding code
+
+another information 
+
+second line
